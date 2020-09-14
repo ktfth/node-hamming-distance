db63da7e1ee863c4052aab82fa85daba6cfb57db
Kaique da Silva <kaeyosthaeron@gmail.com>
2020-09-14 20:13:06 -0300

    Project started

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+const assert = require('assert');
+
+function bitDiffCount(a, b) {
+  const bitStr = (a ^ b >>> 0).toString(2);
+  return bitStr.split('1').length - 1;
+}
+
+function hammingDistance(a, b) {
+  let out = 0;
+  a = Buffer.from(a, 'binary');
+  b = Buffer.from(b, 'binary');
+  for (let i = 0; i < a.length; i += 1) {
+    out += bitDiffCount(a[i], b[i]);
+  }
+  return out;
+}
+module.exports = exports = hammingDistance;
+
+assert.equal(hammingDistance('this is a test', 'wokka wokka!!!'), 37);
